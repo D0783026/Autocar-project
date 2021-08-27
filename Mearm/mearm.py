fb978d8956fd6d6c89dabf609eee856334409331
楊寧 <yyaoning@gmail.com>
2021-08-27 21:04:41 +0800

    8/27 mearm check

--- Mearm/mearm.py
+++ Mearm/mearm.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+import curses
  
 CONTROL_PIN = 14
 PWM_FREQ = 50
@@ -9,8 +10,9 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(CONTROL_PIN, GPIO.OUT)
  
 pwm = GPIO.PWM(CONTROL_PIN, PWM_FREQ)
-pwm.start(90)
- 
+pwm.start(0)
+stdrc = curses.initscr()
+stdrc.clear()
 def angle_to_duty_cycle(angle=0):
     duty_cycle = (0.05 * PWM_FREQ) + (0.19 * PWM_FREQ * angle / 180)
     return duty_cycle
@@ -19,32 +21,17 @@ def angle_to_duty_cycle(angle=0):
 
 
 if __name__ == '__main__':
-    flag=0
-    ch = input()
+    angle=0
+    
     while True:
-        if ch=='j':
-            for angle in range(90,180,STEP):
-                if ch=='p':
-                    print('結束程式')
-                    flag=1
-                    break
-                dc = angle_to_duty_cycle(angle) 
-                pwm.ChangeDutyCycle(dc)
-                print('角度={: >3}, 工作週期={:.2f}'.format(angle, dc))
-                time.sleep(0.5)
-        elif ch=='l':
-            for angle in range(90,-1,STEP):
-                if ch=='p':
-                    print('結束程式')
-                    flag=1
-                    break
-                dc = angle_to_duty_cycle(angle) 
-                pwm.ChangeDutyCycle(dc)
-                print('角度={: >3}, 工作週期={:.2f}'.format(angle, dc))
-                time.sleep(0.5)
+        angle+=1
+        ch = stdrc.getkey()
+        if ch=='a':
+            dc = angle_to_duty_cycle(angle) 
+            pwm.ChangeDutyCycle(dc)
+            print('角度={: >3}, 工作週期={:.2f}'.format(angle, dc))
+            
                
-        if flag==0:
-            break;
 
     pwm.stop()
     GPIO.cleanup()
--- Wheel/control_speed.py
+++ Wheel/control_speed.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8-*-
-
+import enum
 import RPi.GPIO as GPIO
 import curses
 import time
 from curses import wrapper
+import socket
 
+
+
+#setting GPIO
 GPIO.setmode(GPIO.BCM)
 
 RIN1 = 13       #白
@@ -43,59 +47,122 @@ pwm_R_B = GPIO.PWM(ENR_B,500)  #ENR_B设置为PWM控制
 pwm_L_A = GPIO.PWM(ENL_A,500)  #ENL_A设置为PWM控制
 pwm_L_B = GPIO.PWM(ENL_B,500)  #NL_B设置为PWM控制
 
-pwm_R_A.start(50)
-pwm_R_B.start(50)
-pwm_L_A.start(50)
-pwm_L_B.start(50)
+pwm_R_A.start(100)
+pwm_R_B.start(100)
+pwm_L_A.start(100)
+pwm_L_B.start(100)
 
-class control_:
-    bind_ip = "0.0.0.0"
-    bind_port = 9999
-    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    server.bind((bind_ip,bind_port))
-    server.listen(5)
-    print ("[*] Listening on %s:%d " % (bind_ip,bind_port))
-    def Speed_Control(x):
-        value=0
-        global pwm_L_A, pwm_L_B, pwm_R_A, pwm_R_B
-        if x == 'q':
-            pwm_R_A.ChangeDutyCycle(0)
-            pwm_L_A.ChangeDutyCycle(0)
-            pwm_R_B.ChangeDutyCycle(0)
-            pwm_L_B.ChangeDutyCycle(0)
+#set socket
+bind_ip = "0.0.0.0"
+bind_port = 9999
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+server.bind((bind_ip,bind_port))
+
+server.listen(5)
+
+print ("[*] Listening on %s:%d " % (bind_ip,bind_port))
+
+def Speed_Control(x):
+    value=0
+    global pwm_L_A, pwm_L_B, pwm_R_A, pwm_R_B
+    if x == 'q':
+        pwm_R_A.ChangeDutyCycle(0)
+        pwm_L_A.ChangeDutyCycle(0)
+        pwm_R_B.ChangeDutyCycle(0)
+        pwm_L_B.ChangeDutyCycle(0)
         
 
-        elif x == 'w':
-            pwm_R_A.ChangeDutyCycle(100)
-            pwm_L_A.ChangeDutyCycle(100)
-            pwm_R_B.ChangeDutyCycle(100)
-            pwm_L_B.ChangeDutyCycle(100)
+    elif x == 'w':
+        pwm_R_A.ChangeDutyCycle(100)
+        pwm_L_A.ChangeDutyCycle(100)
+        pwm_R_B.ChangeDutyCycle(100)
+        pwm_L_B.ChangeDutyCycle(100)
         
-        elif x == 'x':
-            pwm_R_A.ChangeDutyCycle(100)
-            pwm_L_A.ChangeDutyCycle(100)
-            pwm_R_B.ChangeDutyCycle(100)
-            pwm_L_B.ChangeDutyCycle(100)
+    elif x == 'x':
+        pwm_R_A.ChangeDutyCycle(100)
+        pwm_L_A.ChangeDutyCycle(100)
+        pwm_R_B.ChangeDutyCycle(100)
+        pwm_L_B.ChangeDutyCycle(100)
     
-        elif x == 'a':
-            pwm_R_A.ChangeDutyCycle(100)
-            pwm_R_B.ChangeDutyCycle(100)
+    elif x == 'a':
+        pwm_R_A.ChangeDutyCycle(100)
+        pwm_R_B.ChangeDutyCycle(100)
         
 
-        elif x == 'd':
+    elif x == 'd':
         
-            pwm_L_A.ChangeDutyCycle(100)
-            pwm_L_B.ChangeDutyCycle(100)
+        pwm_L_A.ChangeDutyCycle(100)
+        pwm_L_B.ChangeDutyCycle(100)
 
-    
+class State(enum):
+    Forward = 1
+    left = 2
 
+state=0
+while True:
+    client,addr = server.accept()
+    print ('Connected by ', addr)
+   
     while True:
-        ch = stdscr.getkey()
-        # Quit
-        if ch == 'q':
-            curses.endwin()
-            Speed_Control(ch)
-       
+        
+        data = client.recv(1024)
+        client.send("ACK!".encode())
+        print (data)
+        if float(data)<=4.0 & float(data) >= 2.0:
+            data_F = float(data)
+        elif float(data) <= 1 & float(data) >=-1:
+            data_r = float(data)
+            
+        if float(data) <= 1.0:
+            # Turn Left
+            if float(data) <= -0.5:
+                GPIO.output(LIN1, GPIO.LOW)
+                GPIO.output(LIN2, GPIO.HIGH)
+                GPIO.output(LIN3, GPIO.LOW)
+                GPIO.output(LIN4, GPIO.HIGH)
+                GPIO.output(RIN1, GPIO.LOW)
+                GPIO.output(RIN2, GPIO.LOW)
+                GPIO.output(RIN3, GPIO.LOW)
+                GPIO.output(RIN4, GPIO.LOW)
+
+            # Turn Right
+            elif float(data) >= 0.5:
+                
+                GPIO.output(LIN1, GPIO.LOW)
+                GPIO.output(LIN2, GPIO.LOW)
+                GPIO.output(LIN3, GPIO.LOW)
+                GPIO.output(LIN4, GPIO.LOW)
+                GPIO.output(RIN1, GPIO.LOW)
+                GPIO.output(RIN2, GPIO.HIGH)
+                GPIO.output(RIN3, GPIO.LOW)
+                GPIO.output(RIN4, GPIO.HIGH)
+                
+        elif float(data)  >= 2.0:
+            # Forward
+            if float(data) <= 2.5:
+                GPIO.output(LIN1, GPIO.HIGH)
+                GPIO.output(LIN2, GPIO.LOW)
+                GPIO.output(LIN3, GPIO.HIGH)
+                GPIO.output(LIN4, GPIO.LOW)
+                GPIO.output(RIN1, GPIO.HIGH)
+                GPIO.output(RIN2, GPIO.LOW)
+                GPIO.output(RIN3, GPIO.HIGH)
+                GPIO.output(RIN4, GPIO.LOW)
+
+            # Backward
+            elif float(data) >= 3.5 :
+                GPIO.output(LIN1, GPIO.LOW)
+                GPIO.output(LIN2, GPIO.HIGH)
+                GPIO.output(LIN3, GPIO.LOW)
+                GPIO.output(LIN4, GPIO.HIGH)
+                GPIO.output(RIN1, GPIO.LOW)
+                GPIO.output(RIN2, GPIO.HIGH)
+                GPIO.output(RIN3, GPIO.LOW)
+                GPIO.output(RIN4, GPIO.HIGH)
+            
+        elif data_F :
             GPIO.output(LIN1, GPIO.LOW)     #GPIO17
             GPIO.output(LIN2, GPIO.LOW)     #GPIO18
             GPIO.output(LIN3, GPIO.LOW)     #GPIO22
@@ -104,60 +171,5 @@ class control_:
             GPIO.output(RIN2, GPIO.LOW)     #GPIO11
             GPIO.output(RIN3, GPIO.LOW)     #GPIO25
             GPIO.output(RIN4, GPIO.LOW)     #GPIO10
-       
-            GPIO.cleanup()       #清除GPIO資料
-      
+            #GPIO.cleanup()       #清除GPIO資料'''
 
-            break
-
-        # Forward
-        elif ch == 'w':
-            Speed_Control(ch)
-            GPIO.output(LIN1, GPIO.LOW)
-            GPIO.output(LIN2, GPIO.HIGH)
-            GPIO.output(LIN3, GPIO.LOW)
-            GPIO.output(LIN4, GPIO.HIGH)
-            GPIO.output(RIN1, GPIO.LOW)
-            GPIO.output(RIN2, GPIO.HIGH)
-            GPIO.output(RIN3, GPIO.LOW)
-            GPIO.output(RIN4, GPIO.HIGH)
-       
-
-        # Backward
-        elif ch == 'x':
-            Speed_Control(ch)
-            GPIO.output(LIN1, GPIO.HIGH)
-            GPIO.output(LIN2, GPIO.LOW)
-            GPIO.output(LIN3, GPIO.HIGH)
-            GPIO.output(LIN4, GPIO.LOW)
-            GPIO.output(RIN1, GPIO.HIGH)
-            GPIO.output(RIN2, GPIO.LOW)
-            GPIO.output(RIN3, GPIO.HIGH)
-            GPIO.output(RIN4, GPIO.LOW)
-
-        # Turn Right
-        elif ch == 'd':
-            Speed_Control(ch)
-            GPIO.output(LIN1, GPIO.LOW)
-            GPIO.output(LIN2, GPIO.HIGH)
-            GPIO.output(LIN3, GPIO.LOW)
-            GPIO.output(LIN4, GPIO.HIGH)
-            GPIO.output(RIN1, GPIO.LOW)
-            GPIO.output(RIN2, GPIO.LOW)
-            GPIO.output(RIN3, GPIO.LOW)
-            GPIO.output(RIN4, GPIO.LOW)
-
-        # Turn Left
-        elif ch == 'a':
-            Speed_Control(ch)
-            GPIO.output(LIN1, GPIO.LOW)
-            GPIO.output(LIN2, GPIO.LOW)
-            GPIO.output(LIN3, GPIO.LOW)
-            GPIO.output(LIN4, GPIO.LOW)
-            GPIO.output(RIN1, GPIO.LOW)
-            GPIO.output(RIN2, GPIO.HIGH)
-            GPIO.output(RIN3, GPIO.LOW)
-            GPIO.output(RIN4, GPIO.HIGH)
-
-
-        GPIO.cleanup()       #清除GPIO資料git pull origin master
