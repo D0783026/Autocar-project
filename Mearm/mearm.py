9eb124c4cf765373d4802534dc86e8084678c033
Sam Yang <sam.yang881009@gmail.com>
2021-10-13 00:24:44 +0800

    整合樹莓派程式

--- Mearm/mearm.py
+++ Mearm/mearm.py
@@ -1,18 +1,44 @@
 import RPi.GPIO as GPIO
 import time
 import curses
- 
-CONTROL_PIN = 14
-PWM_FREQ = 50
-STEP=1
+import sys
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(CONTROL_PIN, GPIO.OUT)
- 
-pwm = GPIO.PWM(CONTROL_PIN, PWM_FREQ)
-pwm.start(0)
-stdrc = curses.initscr()
-stdrc.clear()
+GPIO.setmode(GPIO.BCM) 
+CONTROL_PIN_X = 5
+CONTROL_PIN_Y = 6
+CONTROL_PIN_Z = 22
+CONTROL_PIN_S = 23  #夾子
+PWM_FREQ = 50
+
+angle_X=90
+angle_Y=90
+angle_Z=90
+angle_S=0
+
+
+
+GPIO.setup(CONTROL_PIN_X, GPIO.OUT)
+GPIO.setup(CONTROL_PIN_Y, GPIO.OUT)
+GPIO.setup(CONTROL_PIN_Z, GPIO.OUT)
+GPIO.setup(CONTROL_PIN_S, GPIO.OUT)
+
+pwm_X = GPIO.PWM(CONTROL_PIN_X, PWM_FREQ)
+pwm_Y = GPIO.PWM(CONTROL_PIN_Y, PWM_FREQ)
+pwm_Z = GPIO.PWM(CONTROL_PIN_Z, PWM_FREQ)
+pwm_S = GPIO.PWM(CONTROL_PIN_S, PWM_FREQ)
+
+
+def open_all():
+   # global pwm_X, pwm_Y, pwm_Z, pwm_S
+    print('ok')
+    
+    pwm_Y.start(100)
+    pwm_X.start(100)
+    pwm_Z.start(100)
+    pwm_S.start(100)
+    
+    
+    
 def angle_to_duty_cycle(angle=0):
     duty_cycle = (0.05 * PWM_FREQ) + (0.19 * PWM_FREQ * angle / 180)
     return duty_cycle
@@ -20,18 +46,111 @@ def angle_to_duty_cycle(angle=0):
 
 
 
-if __name__ == '__main__':
-    angle=0
-    
-    while True:
-        angle+=1
-        ch = stdrc.getkey()
-        if ch=='a':
-            dc = angle_to_duty_cycle(angle) 
-            pwm.ChangeDutyCycle(dc)
-            print('角度={: >3}, 工作週期={:.2f}'.format(angle, dc))
-            
-               
 
-    pwm.stop()
-    GPIO.cleanup()
+
+def Base(x):
+    global angle_X
+    
+    if x <= -0.5:
+        dc = angle_to_duty_cycle(angle_X) 
+        pwm_X.ChangeDutyCycle(dc)
+        dc=0
+        
+        if angle_X>=180:
+            pass
+        else:
+            angle_X+=3
+        return True
+        
+    elif x >= 0.5:
+        dc = angle_to_duty_cycle(angle_X) 
+        pwm_X.ChangeDutyCycle(dc)
+        dc=0
+        
+        if angle_X<=0:
+            pass
+        else:
+            angle_X-=3
+        return True
+    
+    else:
+        return False
+    
+    
+    
+def control_Forward_Backward(x):
+    global angle_Y
+    if x <= -0.5: 
+        dc = angle_to_duty_cycle(angle_Y) 
+        pwm_Y.ChangeDutyCycle(dc)
+        dc=0
+        if angle_Y>=180:
+            pass
+        else:
+            angle_Y+=3
+        return True
+    
+    elif x >= 0.5:
+        
+        dc = angle_to_duty_cycle(angle_Y) 
+        pwm_Y.ChangeDutyCycle(dc)
+        dc=0
+        if angle_Y<=0:
+            pass
+        else:
+            angle_Y-=3
+        return True
+    else:
+        return False
+    
+    
+    
+
+def control_High_Low(but_Y,but_A):
+    global angle_Z
+    if but_Y == 1 and but_A == 0: 
+        dc = angle_to_duty_cycle(angle_Z) 
+        pwm_Z.ChangeDutyCycle(dc)
+        dc=0
+        if angle_Z>=180:
+            pass
+        else:
+            angle_Z+=3
+        return True
+    
+    elif but_A == 1 and but_Y == 0:
+        dc = angle_to_duty_cycle(angle_Z) 
+        pwm_Z.ChangeDutyCycle(dc)     
+        dc=0
+        if angle_Z<=0:
+            pass
+        else:
+            angle_Z-=3
+        return True
+    else:
+        return False
+    
+    
+
+def control_Scissor(but_X,but_B):
+    global angle_S
+    if but_X == 1 : 
+        angle_S=180
+        dc = angle_to_duty_cycle(angle_S) 
+        pwm_S.ChangeDutyCycle(dc)
+        dc=0
+        return True
+    
+    elif but_B == 1:
+        angle_S=0
+        dc = angle_to_duty_cycle(angle_S) 
+        pwm_S.ChangeDutyCycle(dc)       
+        dc=0
+        return True
+    else:
+        return False
+    
+    
+    
+    
+
---
+++ Wheel/controlWheel.py
@@ -0,0 +1,118 @@
+from enum import Enum
+import RPi.GPIO as GPIO
+
+#setting GPIO
+GPIO.setmode(GPIO.BCM)
+
+RIN1 = 13       #白
+RIN2 = 16       #橘
+RIN3 = 19       #灰
+RIN4 = 20       #紫
+LIN1 = 15        #紅
+LIN2 = 4       #藍
+LIN3 = 17       #黃
+LIN4 = 18       #綠
+
+ENR_A = 12
+ENR_B = 21
+ENL_A = 14
+ENL_B = 27
+
+GPIO.setup(RIN1, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(RIN2, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(RIN3, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(RIN4, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LIN1, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LIN2, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LIN3, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LIN4, GPIO.OUT, initial=GPIO.LOW)
+
+GPIO.setup(ENR_A, GPIO.OUT)
+GPIO.setup(ENR_B, GPIO.OUT)
+GPIO.setup(ENL_A, GPIO.OUT)
+GPIO.setup(ENL_B, GPIO.OUT)
+
+pwm_R_A = GPIO.PWM(ENR_A,500)  #ENR_A设置为PWM控制
+pwm_R_B = GPIO.PWM(ENR_B,500)  #ENR_B设置为PWM控制
+pwm_L_A = GPIO.PWM(ENL_A,500)  #ENL_A设置为PWM控制
+pwm_L_B = GPIO.PWM(ENL_B,500)  #NL_B设置为PWM控制
+
+pwm_R_A.start(100)
+pwm_R_B.start(100)
+pwm_L_A.start(100)
+pwm_L_B.start(100)
+
+#控制前後
+def control_forward(x):
+
+    #forward
+    if x <= -0.5:
+        GPIO.output(LIN1, GPIO.HIGH)
+        GPIO.output(LIN2, GPIO.LOW)
+        GPIO.output(LIN3, GPIO.HIGH)
+        GPIO.output(LIN4, GPIO.LOW)
+        GPIO.output(RIN1, GPIO.LOW)
+        GPIO.output(RIN2, GPIO.HIGH)
+        GPIO.output(RIN3, GPIO.LOW)
+        GPIO.output(RIN4, GPIO.HIGH)
+        print("forward")
+        return True
+
+    #backward
+    elif x >= 0.5:
+        GPIO.output(LIN1, GPIO.LOW)
+        GPIO.output(LIN2, GPIO.HIGH)
+        GPIO.output(LIN3, GPIO.LOW)
+        GPIO.output(LIN4, GPIO.HIGH)
+        GPIO.output(RIN1, GPIO.HIGH)
+        GPIO.output(RIN2, GPIO.LOW)
+        GPIO.output(RIN3, GPIO.HIGH)
+        GPIO.output(RIN4, GPIO.LOW)
+        return True
+
+    else: 
+        return False
+
+
+#控制左右
+def control_left(x):
+
+    #left
+    if x <= -0.5:
+        GPIO.output(LIN1, GPIO.LOW)
+        GPIO.output(LIN2, GPIO.HIGH)
+        GPIO.output(LIN3, GPIO.LOW)
+        GPIO.output(LIN4, GPIO.HIGH)
+        GPIO.output(RIN1, GPIO.LOW)
+        GPIO.output(RIN2, GPIO.HIGH)
+        GPIO.output(RIN3, GPIO.LOW)
+        GPIO.output(RIN4, GPIO.HIGH)
+        return True
+
+    #right
+    elif x >= 0.5:
+        GPIO.output(LIN1, GPIO.HIGH)
+        GPIO.output(LIN2, GPIO.LOW)
+        GPIO.output(LIN3, GPIO.HIGH)
+        GPIO.output(LIN4, GPIO.LOW)
+        GPIO.output(RIN1, GPIO.HIGH)
+        GPIO.output(RIN2, GPIO.LOW)
+        GPIO.output(RIN3, GPIO.HIGH)
+        GPIO.output(RIN4, GPIO.LOW)
+        return True
+
+    else: 
+        return False
+
+
+#停止輪胎動作
+def stop():
+    GPIO.output(LIN1, GPIO.LOW)     #GPIO17
+    GPIO.output(LIN2, GPIO.LOW)     #GPIO18
+    GPIO.output(LIN3, GPIO.LOW)     #GPIO22
+    GPIO.output(LIN4, GPIO.LOW)     #GPIO23
+    GPIO.output(RIN1, GPIO.LOW)     #GPIO7
+    GPIO.output(RIN2, GPIO.LOW)     #GPIO11
+    GPIO.output(RIN3, GPIO.LOW)     #GPIO25
+    GPIO.output(RIN4, GPIO.LOW)     #GPIO10
+    
